4bd511328815dd2444d4feecd4a9699e8158a704
Zuke <zgaming705@gmail.com>
2019-12-21 17:05:30 -0500

    YouTube API & Updates

---
+++ Meep/commands/util/helpme.js
@@ -0,0 +1,25 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+
+module.exports = {
+    name: "helpme",
+    category: "util",
+    run: async (client, message, args) => {
+        if(message.deletable) message.delete();
+
+        if(!args[0]) return message.channel.send("Please provide a reason for the help").then(m => m.delete(5000));
+
+        const channel = message.guild.channels.find(channel => channel.name === "support-requests");
+        if(!channel) return message.channel.send("Could not find a \`#support-requests\` channel");
+
+        const embed = new RichEmbed()
+            .setColor("#ffb982")
+            .setTimestamp()
+            .setFooter(message.guild.name, message.guild.iconURL)
+            .setAuthor("Help Requested", message.author.displayAvatarURL)
+            .addField("Reason", args.slice(1).join(" "))
+            .addField("Channel", `\`#${message.channel}\``);
+
+        await channel.send(embed);
+    }
+};
--- Meep/index.js
+++ Meep/index.js
@@ -5,6 +5,8 @@ const { stripIndents } = require("common-tags");
 let xp = require("./xp.json");
 let functions = require("./functions.js");
 
+//YouTube API: AIzaSyAamO7NK8d6nwSZ75sl2ks5vhpnwObuVxI
+
 client.commands = new Collection();
 client.aliases = new Collection();
 
